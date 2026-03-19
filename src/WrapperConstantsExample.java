fe889cdb193205f9e7a0862030139b154ef2e6be
양은찬 <yangeun3209@gmail.com>
2026-03-19 17:45:21 +0900

    3번째 실습 추가

---
+++ src/WrapperConstantsExample.java
@@ -0,0 +1,10 @@
+public class WrapperConstantsExample {
+    public static void main(String[] args) {
+        System.out.println("int MAX_VALUE: " + Integer.MAX_VALUE);
+        System.out.println("int MIN_VALUE: " + Integer.MIN_VALUE);
+        System.out.println("long MAX_VALUE: " + Long.MAX_VALUE);
+        System.out.println("long MIN_VALUE: " + Long.MIN_VALUE);
+        System.out.println("double MAX_VALUE: " + Double.MAX_VALUE);
+        System.out.println("double MIN_VALUE: " + Double.MIN_VALUE);
+    }
+}
