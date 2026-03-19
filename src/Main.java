84cd4980cd23a22aa89bf1bfa7d439b82300bd3e
양은찬 <yangeun3209@gmail.com>
2026-03-19 17:40:42 +0900

    2번째 실습 내용 추가

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,17 @@ public class Main {
         System.out.println("float: " + (Float.SIZE / 8) + " bytes");
         System.out.println("double: " + (Double.SIZE / 8) + " bytes");
         System.out.println("char: " + (Character.SIZE / 8) + " bytes");
+
+        // int 타입의 최댓값과 최솟값 출력
+        System.out.println("int 최대값: " + Integer.MAX_VALUE);
+        System.out.println("int 최소값: " + Integer.MIN_VALUE);
+
+        // long 타입의 최댓값과 최솟값 출력
+        System.out.println("long 최대값: " + Long.MAX_VALUE);
+        System.out.println("long 최소값: " + Long.MIN_VALUE);
+
+        // double 타입의 최댓값과 최솟값 출력
+        System.out.println("double 최대값: " + Double.MAX_VALUE);
+        System.out.println("double 최소값: " + Double.MIN_VALUE);
     }
 }
